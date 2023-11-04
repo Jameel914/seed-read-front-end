20c40460990f1bb09dc1f3d32879aa14302f238b
jameel914 <jameelibrahim107@gmail.com>
2023-11-04 11:54:22 -0400

    components created and router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import EditPage from "./Components/EditPage";
+import IndexPage from "./Components/IndexPage";
+import Navbar from "./Components/Navbar";
+import NewPage from "./Components/NewPage";
+import ShowPage from "./Components/ShowPage";
 import "./App.css";
 
 function App() {
-  return <div>Front end</div>;
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/songs" element={<IndexPage />} />
+        <Route path="/songs/new" element={<NewPage />} />
+        <Route path="/songs/:id" element={<ShowPage />} />
+        <Route path="/songs/:id/edit" element={<EditPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/Components/ShowPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ShowPage() {
+  return <div>ShowPage</div>;
+}
+
+export default ShowPage;
