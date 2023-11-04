dfdc3bf3db58965e48a6de50ecd8387f57dd8ddd
jameel914 <jameelibrahim107@gmail.com>
2023-11-04 12:31:20 -0400

    installed axios and added env

--- src/Components/IndexPage.jsx
+++ src/Components/IndexPage.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+// import React, { useEffect, useState } from "react";
+// import { useNavigate } from "react-router-dom";
+// import axios from "axios";
 
-function IndexPage() {
-  return <div>IndexPage</div>;
-}
+// function IndexPage() {
+//     const [data, setData] = useState([])
+//     const API = import.meta.env.VITE_API_URL
+// async function allSongs() {
+//     let result = await axios.get()
+// }
+//   return <div>IndexPage</div>;
+// }
 
-export default IndexPage;
+// export default IndexPage;
